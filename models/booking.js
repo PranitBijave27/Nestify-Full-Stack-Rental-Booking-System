dc3c66ca51880f24f03239c2da5814b813e61e89
Pranit <pranitbijave27@gmail.com>
2026-03-04 12:36:45 +0530

    refactored code

--- models/booking.js
+++ models/booking.js
@@ -1,4 +1,3 @@
-const { required } = require("joi");
 const mongoose=require("mongoose");
 const Schema=mongoose.Schema;
 
@@ -35,3 +34,4 @@ const bookingSchema=new Schema({
         default: Date.now
     }
 });
+
